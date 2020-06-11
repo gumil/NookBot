02cdffdb02febd1301be7a4d224f7a7f0c1328ac
Miguel Panelo <miguelrpanelo@gmail.com>
2020-06-11 10:49:15 +0200

    Remove limit parameter

--- core/src/main/kotlin/dev/gumil/nookbot/telegram/TelegramApiImpl.kt
+++ core/src/main/kotlin/dev/gumil/nookbot/telegram/TelegramApiImpl.kt
@@ -13,10 +13,9 @@ internal class TelegramApiImpl(
     private val baseUrl = "https://api.telegram.org/bot$token/"
     private val getUpdates = "getUpdates"
 
-    override suspend fun getUpdates(limit: Int, offset: Int): List<Update> {
+    override suspend fun getUpdates(offset: Int): List<Update> {
         val urlString = baseUrl + getUpdates
-        val query = "?limit=$limit" +
-                "&offset=$offset" +
+        val query = "?offset=$offset" +
                 "&allowed_updates=[message]"
         val httpResponse = httpClient.get<HttpResponse<List<Update>>>(
             urlString + query
